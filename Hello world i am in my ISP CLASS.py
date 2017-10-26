3f7dac72cc53d00a7ff6f70c3069e2c8acc92371
CisseBE <33008046+CisseBE@users.noreply.github.com>
2017-10-26 15:28:22 +0100

    Add files via upload

---
+++ Hello world i am in my ISP CLASS.py
@@ -0,0 +1,7 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: C:/Users/21357349/inro to software wek 2.py ============
+Hello World
+I am in my ISD class right now
+>>> 
