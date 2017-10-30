3a6dc881f23286c3614e5e2c4f2d9b6ec34b536c
CisseBE <33008046+CisseBE@users.noreply.github.com>
2017-10-30 17:51:00 +0000

    Add files via upload

---
+++ week3 task 1.PY.py
@@ -0,0 +1,5 @@
+number1=int
+number2=int
+number1 = int(input("Enter your first number:"))
+number2 = int(input("Enter your second number:"))
+print("The total of your numbers are", "\t", number1+number2)
