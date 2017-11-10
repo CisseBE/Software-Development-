df1f76def0ed19b6f334ad0737cfd470f4890353
CisseBE <33008046+CisseBE@users.noreply.github.com>
2017-11-10 20:39:08 +0000

    Add files via upload

---
+++ shoppingcart.py
@@ -0,0 +1,11 @@
+print("please enter your shopping items, when done please press enter.")
+
+shoppingcart = []
+
+entered = input("Please enter your first shopping item:")
+
+while entered !="":
+    shoppingcart.append(entered)
+    entered = input("Enter another shopping item please")
+
+print("Thank you, your shoppingcart is:",shoppingcart)
